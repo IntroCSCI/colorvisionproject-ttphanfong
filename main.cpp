a54ba5d910cf70d2afcd07d9d016bf8f7c9290b8
Travis Phan-Fong <ttphan-fong@mail.csuchico.edu>
2021-03-12 16:51:20 +0000

    started

--- main.cpp
+++ main.cpp
@@ -1,9 +1,25 @@
-//Author: PUT YOUR NAME HERE
+//Author: Travis Phan-Fong
 #include <iostream>
+#include <fstream>
 using namespace std;
+
+///
+/// GOAL: Pallette Generator CSS
+///
 
 int main()
 {
+  int num;
+  string filename;
+
+  cout << endl <<"Enter needed number of colours:" <<endl;
+  cin >> num;
+  cout << "Enter name of file to save to (.txt):"<<endl;
+  cin >> filename;
+  fstream savedFile(filename);
+  savedFile << num;
+  cout << endl<< "Saved to " << filename << " with " << num << " colours chosen.";
+  savedFile.close();
 
   return 0;
 }
